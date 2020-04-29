1e3851cc5c3840a1394b886fe36a4b5d17c1b546
Jun Honda <honjetman@gmail.com>
2020-04-29 23:22:50 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+
+  devise_for(
+    :users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout'
+    },
+    controllers: {
+      registrations: 'users/registrations',
+      sessions: 'users/sessions'
+    }
+  )
+
+  resources :articles, only: %w(index show)
+  namespace :user_page, path: :user, as: :user do
+    root 'articles#index'
+    resources :articles
+  end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
