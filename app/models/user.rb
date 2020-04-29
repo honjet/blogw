5ae1ad7940a608c4f703cb4c8c8dd7a39b5be98d
Jun Honda <honjetman@gmail.com>
2020-04-29 23:24:20 +0900

    Generate devise User files

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+
+  validates :username, presence: true, uniqueness: true
+
+  has_many :articles, dependent: :destroy
+end
